68a35b81f401c9e9f754f1344b0b8896d99e9047
Avinash Rouniyar <www.avinashrouniyar.com@gmail.com>
2021-04-23 17:25:22 +0545

    Update Among Us Character Drawing with Python Turtle.py

--- Among Us Character Drawing with Python Turtle.py
+++ Among Us Character Drawing with Python Turtle.py
@@ -1,37 +1,4 @@
-'''Among Us Character Drawing with Python Turtle
-
-Explanation:
-
-First Part:
-a. First, import the turtle module. Then, set the value of a variable “color1” as “yellow”,”color2″ as “”, “color3” as “sky-blue” and “color4” as “”. Likewise, create an instance of the turtle and store it in “t”. Create a screen for the turtle and store it in “s”.
-
-The Body as Jiu() function:
-a. Create a function named jiu(). Inside this function, set the pen size of the turtle as 20. Fill the color as in “color1” and begin the fill.
-b. Set the turtle right at an angle of 90 degrees and move the turtle forward at an angle of 50 units. Set the turtle right at an angle of 180 degrees and create a circle with the arguments (40, -180).
-c. Again, set the turtle right at an angle of 180 degrees and forward at 200 units.
-d. Set the turtle right at an angle of 180 degrees and create another circle with the arguments (100, -180)
-e. Move the turtle backward at 20 units and set it left at an angle of 15 degrees. Create a circle with the arguments (500, -20) and move it backward at 20 units.
-f. Similarly, create another circle at 40, -180 and set it left at an angle of 7 degrees. Move the turtle backward at 50 units.
-g. Now, set the turtle up and left at 90 degrees. Move it forward at 10 units and right at 90 degrees. Call the down() method.
-h. Set the turtle right at 240 degrees and create a circle as in 50, -70 and end the fill.
-
-The glasses as glass() function:
-a. Call the up() method and set the turtle right at an angle of 230 degrees. Move the turtle forward at an angle of 100 degrees and set it left at an angle of 90 degrees. Move it forward at 20 units and again set it right at 90 degrees.
-b. Call the down() method and fill the color as in “color3” and begin the fill.
-c. Set the turtle right at an angle of 150 degrees and create a circle of 90, -55.
-d. Move the turtle right at an angle of 180 degrees and move it forward at 1 unit. Again, set the turtle right at an angle of 180 and create a circle as 10, -65. Right at 180 degrees and forward at 110 degrees and again right at 180 degrees.
-e. Accordingly, create another circle as 50, -190 and set the turtle right at 170 degrees and forward at 80 units.
-f. Likewise, set it right at 180 degrees and draw a circle as 45, -30 and end the fill.
-
-The Bag as jhola() function:
-a. Move the turtle up and set it right at 90 degrees. Move it forward at 30 units and right at 255 degrees. Start to fill the color as in “color1” and begin the fill.
-b. Set the turtle down, forward at 30 units, and right at 255 degrees.
-c. Draw a circle as 300, -30 and set it right at 260 degrees and forward at 30 units. End the fill.
-
-Last Part:
-a. Lastly, call all the function which are jiu(), glass() and jhola().
-b. End the code with the turtle.done() method.'''
-
+'''Among Us Character Drawing with Python Turtle'''
 
 import turtle
 
